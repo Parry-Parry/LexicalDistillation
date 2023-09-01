ef46f7ab77144b15b3cddc2f0c0a398f7ce5b7ca
Andrew Parry <2393265p@student.gla.ac.uk>
2023-09-01 16:00:43 +0100

    should now make pivots

--- lexdistill/evaluate.py
+++ lexdistill/evaluate.py
@@ -6,7 +6,8 @@ from ir_measures import *
 import ir_datasets as irds
 import pandas as pd
 
-def main(eval :str, run_dir : str, out_path : str):
+
+def main(eval :str, run_dir : str, out_dir : str):
     files = [f for f in os.listdir(run_dir) if os.path.isfile(join(run_dir, f))]
     ds = irds.load(eval)
     qrels = pd.DataFrame(ds.qrels_iter())
@@ -17,13 +18,24 @@ def main(eval :str, run_dir : str, out_path : str):
         if file.endswith(".gz"):
             name = file.split(".")[0]
             run = read_trec_run(join(run_dir, file))
-            res = evaluate(run, file)
+            res = evaluate.calc_aggregate(run, file)
             res = {str(k) : v for k, v in res.items()}
             res['name'] = name 
             df.append(res)
+            
+            per_query = []
+            for q_res in evaluator.iter_calc(res):
+                q_res = {str(k) : v for k, v in q_res.items()}
+                q_res['name'] = name
+                per_query.append(q_res)
+            
+            per_query = pd.DataFrame.from_records(per_query)
+            # pivot metric per query_id 
+            per_query = per_query.pivot(index='query_id', columns='measure', values='value')
+            per_query.to_csv(join(out_dir, f"{name}_per_query.tsv", index=False, sep='\t'))
     
     df = pd.DataFrame.from_records(df)
-    df.to_csv(out_path, sep='\t', index=False)
+    df.to_csv(join(out_dir, 'metrics.tsv'), sep='\t', index=False)
 
     return "Success!"
 
