11ada193c71baf2538a2d3ce344501dd52ea1690
Andrew Parry <2393265p@student.gla.ac.uk>
2023-09-06 09:53:48 +0100

    refactor

--- lexdistill/dual_loss_ablation/training/__init__.py
+++
@@ -1,3 +0,0 @@
-from .loader import TeacherLoader, StandardLoader, SingleTeacherLoader, PerfectLoader
-from .loss import MarginMSELoss
-from .wrapper import MonoT5Model, Baseline
--- lexdistill/wrapper.py
+++ lexdistill/wrapper.py
@@ -68,6 +68,41 @@ class Baseline(nn.Module):
     def forward(self, x):
         x['labels'] = self.gen_labels(x['input_ids'])
         return self.model(**x)
+
+class DualMonoT5Model(nn.Module):
+    def __init__(self, model, tokenizer):
+        super().__init__()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = model.to(self.device)
+        self.tokenizer = tokenizer
+
+        self.rel = self.tokenizer.encode('true')[0]
+        self.nrel = self.tokenizer.encode('false')[0]
+    
+    @staticmethod
+    def init():
+        model = T5ForConditionalGeneration.from_pretrained('t5-base')
+        tokenizer = T5Tokenizer.from_pretrained('t5-base')
+        return DualMonoT5Model(model, tokenizer)
+
+    def save_pretrained(self, path):
+        self.model.save_pretrained(path)
+    
+    def gen_labels(self, x):
+        return self.tokenizer(['true' if i % 2 == 0 else 'false' for i in range(len(x))], return_tensors='pt', padding=True).input_ids.to(self.device)
+    
+    def train(self):
+        self.model.train()
+    
+    def parameters(self):
+        return self.model.parameters()
+    
+    def forward(self, x):
+        x['labels'] = self.gen_labels(x['input_ids'])
+        output = self.model(**x)
+        logits = output.logits
+        result = logits[:, 0, (self.rel, self.nrel)]
+        return F.log_softmax(result, dim=1)[:, 0].cpu(), output.loss
         
 
 
