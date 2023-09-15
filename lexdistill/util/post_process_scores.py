9af9f96efb058a64aa32ff7d556da15c453cf077
Andrew Parry <2393265p@student.gla.ac.uk>
2023-09-15 11:24:46 +0100

    new utility

---
+++ lexdistill/util/post_process_scores.py
@@ -0,0 +1,16 @@
+from fire import Fire
+import numpy as np 
+import pandas as pd 
+import json 
+from collections import defaultdict
+def main(score_path : str):
+    with open(score_path) as f:
+        scores = json.load(f)
+    
+    new = defaultdict(lambda : defaultdict(lambda : 0.))
+    for k, v in scores.items():
+        for _k, _v in v.items():
+            new[k][_k] = float(_v) if not np.isnan(_v) else 0.
+    
+    with open(score_path, 'w') as f:
+        json.dump(new, f)
