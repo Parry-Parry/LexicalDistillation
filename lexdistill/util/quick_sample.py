09c68b29c4c7ea7d21634ac75f3b326f2e66e8e4
Andrew Parry <2393265p@student.gla.ac.uk>
2023-10-13 09:49:16 +0100

    time to sample

--- lexdistill/util/quick_sample.py
+++ lexdistill/util/quick_sample.py
@@ -13,15 +13,15 @@ def convert_to_dict(result):
 
 clean = lambda x : re.sub(r"[^a-zA-Z0-9¿]+", " ", x)
 
-def main(out_dir : str, triples_file : str = None, subset : int = 100000):
+def main(out_path : str, triples_path : str = None, subset : int = 100000):
     dataset = irds.load("msmarco-passage/train/triples-small")
-    if triples_file:
-        train = pd.read_csv(triples_file, sep='\t', dtype={'qid':str, 'doc_id_a':str, 'doc_id_b': str}, index_col=False)
+    if triples_path:
+        train = pd.read_csv(triples_path, sep='\t', dtype={'qid':str, 'doc_id_a':str, 'doc_id_b': str}, index_col=False)
     else:
         train = pd.DataFrame(dataset.docpairs_iter()).rename(columns={'query_id': 'qid',})
     train = train.sample(n=subset) 
 
-    train.to_csv(out_dir, sep='\t', index=False)
+    train.to_csv(out_path, sep='\t', index=False)
 
     return "Done!"
 
