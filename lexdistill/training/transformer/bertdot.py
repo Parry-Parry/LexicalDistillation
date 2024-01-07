027cf45ee12c210d5e975d8282dd66709a3ad93d
Andrew Parry <2393265p@student.gla.ac.uk>
2024-01-07 10:57:20 +0000

    Further fixes

--- lexdistill/training/transformer/bertdot.py
+++ lexdistill/training/transformer/bertdot.py
@@ -1,6 +1,8 @@
 from pyterrier_dr import HgfBiEncoder, BiScorer
+from transformers import AutoTokenizer
 from typing import Any 
 
 def make_scorer(model : Any, batch_size : int = 64, verbose : bool = False):
-    val_backbone = HgfBiEncoder(model, model.tokenizer, {}, device=model.device)
+    tokenizer = AutoTokenizer.from_pretrained('google/electra-base-discriminator')
+    val_backbone = HgfBiEncoder(model.encoder, tokenizer, {}, device=model.device)
     return BiScorer(val_backbone, batch_size=batch_size, verbose=verbose)
