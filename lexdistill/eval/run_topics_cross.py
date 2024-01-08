99a6ce4c181e10af1993ee51dd60b8fa7100e1db
Andrew Parry <2393265p@student.gla.ac.uk>
2024-01-08 13:56:46 +0000

    add legacy support

--- lexdistill/eval/run_topics_cross.py
+++ lexdistill/eval/run_topics_cross.py
@@ -5,13 +5,24 @@ import os
 from os.path import join
 from pyterrier_dr import ElectraScorer
 
-def main(eval : str, run_dir : str, out_dir : str, baseline : str = None, model : str = None, index : str = 'msmarco_passage', dataset : str = 'irds:msmarco-passage/train/triples-small'):  
+def filename(*args, old=False):
+    if old: return join(*args, 'model')
+    return join(*args, 'model', 'classifier')
+
+def main(eval : str, 
+         run_dir : str, 
+         out_dir : str, 
+         baseline : str = None, 
+         model : str = None, 
+         index : str = 'msmarco_passage', 
+         dataset : str = 'irds:msmarco-passage/train/triples-small',
+         old : bool = False):  
     dataset = pt.get_dataset(dataset)
     bm25 = pt.BatchRetrieve(pt.get_dataset(index).get_index("terrier_stemmed_text"), wmodel="BM25")
     eval = pt.get_dataset(eval)
     os.makedirs(out_dir, exist_ok=True)
     if baseline : 
-        baseline = bm25 >> pt.text.get_text(dataset, "text") >> ElectraScorer(model_name=join(baseline, 'model', 'classifier'))
+        baseline = bm25 >> pt.text.get_text(dataset, "text") >> ElectraScorer(model_name=filename(baseline, old=old))
         if not os.path.exists(join(out_dir, "baseline_run.gz")):
             res = baseline.transform(eval.get_topics())
             pt.io.write_results(res, join(out_dir, "baseline_run.gz"))
@@ -21,7 +32,7 @@ def main(eval : str, run_dir : str, out_dir : str, baseline : str = None, model 
             if os.path.exists(join(out_dir, f"{store}_run.gz")):
                 continue
             try:
-                _model = bm25 >> pt.text.get_text(dataset, "text") >> ElectraScorer(model_name=join(run_dir, store, 'model', 'classifier'))
+                _model = bm25 >> pt.text.get_text(dataset, "text") >> ElectraScorer(model_name=filename(run_dir, store, old=old))
             except OSError:
                 print(f"Failed to load {store}")
                 continue
@@ -29,7 +40,7 @@ def main(eval : str, run_dir : str, out_dir : str, baseline : str = None, model 
             pt.io.write_results(res, join(out_dir, f"{store}_run.gz"))
             del _model
     else:
-        _model = bm25 >> pt.text.get_text(dataset, "text") >> ElectraScorer(model_name=join(run_dir, model, 'model', 'classifier'))
+        _model = bm25 >> pt.text.get_text(dataset, "text") >> ElectraScorer(model_name=filename(run_dir, model, old=old))
         res = _model.transform(eval.get_topics())
         pt.io.write_results(res, join(out_dir, f"{model}_run.gz"))
     return "Success!"
